2b4af6995934b2474901470353433e06291a2973
Emmanuel <polosetroc@gmail.com>
2022-05-23 09:42:39 -0700

    Changed form tag to b-form tag and changed styling of upload file button.

--- client/src/components/UploadImageForm.vue
+++ client/src/components/UploadImageForm.vue
@@ -1,24 +1,30 @@
 <template>
-  <form
-      method=post
-      enctype=multipart/form-data
-      @submit.prevent="onUpload($event)"
+  <b-form method="post"
+          enctype=multipart/form-data
+          @submit="onUpload($event)"
+          ref="form"
   >
-    <input
-        type=file
-        accept="image/*"
-        name=image
-        id="file-input"
-    >
-    <br><br>
-    <input
-        type=submit
-        value="Get prediction"
-    >
-  </form>
+    <b-button-group vertical>
+      <b-button class="fileButton">
+        <label class="custom-file-upload">
+          Select file
+          <input
+              size="60"
+              type=file
+              accept="image/*"
+              name=image
+          >
+        </label>
+      </b-button>
+      <b-button type="submit">
+        Get Prediction
+      </b-button>
+    </b-button-group>
+  </b-form>
 </template>
 
 <script setup lang='ts'>
+import {BButton, BButtonGroup, BForm} from "bootstrap-vue-3";
 import {uploadImageFile} from '@/api-request'
 
 const emit = defineEmits(['predictionResponse']);
@@ -30,3 +36,19 @@ async function onUpload(event: any) {
   }
 }
 </script>
+
+<style scoped>
+.fileButton {
+  padding-inline: 0;
+}
+
+input[type="file"] {
+  display: none;
+}
+
+.custom-file-upload {
+  display: inline-block;
+  cursor: pointer;
+  padding: 0 2rem;
+}
+</style>
