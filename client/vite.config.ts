f695760934fecebca57ca2cdc3a931021c1bda6f
Emmanuel <polosetroc@gmail.com>
2022-05-23 10:38:12 -0700

    Updated vite config and gitignore

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,6 @@
-import { fileURLToPath, URL } from "url";
+import {fileURLToPath, URL} from "url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
@@ -11,4 +11,8 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  build: {
+    assetsDir: './',
+    outDir: '../static/'
+  }
 });
